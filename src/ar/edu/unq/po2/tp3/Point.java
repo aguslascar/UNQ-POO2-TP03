742f31b116f122f54b8330348dc34705a3aef097
Agustin Lascar <aguslascar@hotmail.com.ar>
2022-04-15 19:27:32 -0300

    Ejercicio 7 - Punto

---
+++ src/ar/edu/unq/po2/tp3/Point.java
@@ -0,0 +1,35 @@
+package ar.edu.unq.po2.tp3;
+
+public class Point {
+	private int x;
+	private int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void crearPunto() {
+		this.setX(0);
+		this.setY(0);
+	}
+	
+	public void moverPunto(int newX, int newY) {
+		this.setX(newX);
+		this.setY(newY);
+	}
+	
+	public Point sumarPuntos(Point punto1, Point punto2) {
+		Point nuevoPunto = new Point();
+		nuevoPunto.setX(punto1.getX() + punto2.getX());
+		nuevoPunto.setY(punto1.getY() + punto2.getY());
+		return nuevoPunto;
+	}
+}
